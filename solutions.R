977803bc6cf83ef8c48e5a836f91d637abd0bf1b
Anupam Majhi <majhi.anupam@gmail.com>
2017-08-02 08:50:45 +0530

    Line update

--- solutions.R
+++ solutions.R
@@ -72,3 +72,27 @@ mean(master_frame$raised_amount_usd[which(master_frame$funding_round_type == "an
 mean(master_frame$raised_amount_usd[which(master_frame$funding_round_type == "private_equity")],na.rm = T)
 
 
+
+
+
+#============ GOAL 2 : Country analysis ===========================
+
+
+# Checkpoint 3: Country Analysis
+
+
+
+# Filter by chosen investment type
+filter_by_inv_type <- filter(master_frame,funding_round_type == "venture")
+
+# Group by country code
+group_by_country <- group_by(filter_by_inv_type,country_code)
+
+# Find sum of investment based on country code
+total_investment_by_country <- summarise(group_by_country,Total_Investment = sum(raised_amount_usd,na.rm = T))
+
+
+# For the chosen investment type, make a data frame named top9 with the top nine countries (based on the total investment amount each country has received)
+# Also cleanup Blank Country Code
+top_9 <- arrange(total_investment_by_country[which(total_investment_by_country$country_code != ""),],desc(Total_Investment))[1:9,]
+
